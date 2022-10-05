df1c2173a57c717a8ce6512d1d4a068fa11b607f
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2022-10-05 17:48:03 +0900

    feat: enable `printf()` to display serial console

--- stm32/Core/Src/main.c
+++ stm32/Core/Src/main.c
@@ -72,6 +72,7 @@ void StartDefaultTask(void *argument);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+#include <stdio.h>
 
 /* USER CODE END 0 */
 
@@ -107,6 +108,7 @@ int main(void)
   MX_USB_OTG_FS_PCD_Init();
   MX_USART6_UART_Init();
   /* USER CODE BEGIN 2 */
+  setbuf(stdout, NULL);
 
   /* USER CODE END 2 */
 
@@ -380,7 +382,11 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-
+int _write(int file, char *ptr, int len)
+{
+  HAL_UART_Transmit(&huart3,(uint8_t *)ptr,len,10);
+  return len;
+}
 /* USER CODE END 4 */
 
 /* USER CODE BEGIN Header_StartDefaultTask */
